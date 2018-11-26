aad6b7909f7ec5a32b9c9b7181e73ac86ca692bd
Wileen Chiu <wileenc@codeaurora.org>
2018-11-26 12:08:55 -0800

    bacon: Add support for telephony hidl wrapper
    Change-Id: I4e248bb6de482d503e672d284189b24cafac8d20

--- msm8974.mk
+++ msm8974.mk
@@ -265,8 +265,10 @@ PRODUCT_PACKAGES += \
 # Telephony
 PRODUCT_PACKAGES += \
     ims-ext-common \
-    telephony-ext \
     ims_ext_common.xml \
+    telephony-ext \
+    qti-telephony-hidl-wrapper \
+    qti_telephony_hidl_wrapper.xml \
     qti-telephony-common
 
 # Call the proprietary setup
